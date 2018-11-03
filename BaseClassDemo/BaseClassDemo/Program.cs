19aaf745b040c720c6c3d085d3fbcf840d3acc32
jamesbassett02 <bassettj@uw.edu>
2018-11-03 16:03:17 -0700

    Initial Commit

---
+++ BaseClassDemo/BaseClassDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BaseClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Base Class Demo");
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
